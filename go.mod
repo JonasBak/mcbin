5f6e6c3840e54da1cb9dad8141d4920988f88711
Jonas Bakken <jonasxb@gmail.com>
2020-04-09 23:38:37 +0200

    Allow editing of existing pastes

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/JonasBak/mcbin
 go 1.12
 
 require (
-	github.com/google/uuid v1.1.1
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/graph-gophers/graphql-go v0.0.0-20191115155744-f33e81362277
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
-	"github.com/google/uuid"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/minio/minio-go/v6"
@@ -13,6 +16,29 @@ import (
 	"os"
 	"time"
 )
+
+func randomString(n uint) string {
+	b := make([]byte, n)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func signString(s, secret string) string {
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write([]byte(s))
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+func validate(s, secret, v string) bool {
+	toValidate, err := base64.StdEncoding.DecodeString(v)
+	if err != nil {
+		return false
+	}
+	mac := hmac.New(sha256.New, []byte(secret))
+	mac.Write([]byte(s))
+	expected := mac.Sum(nil)
+	return hmac.Equal(toValidate, expected)
+}
 
 func getMinioClient() *minio.Client {
 	mc, err := minio.New(os.Getenv("MINIO_HOST"), os.Getenv("MINIO_ACCESS"), os.Getenv("MINIO_SECRET"), os.Getenv("MINIO_INSECURE") != "true")
@@ -28,30 +54,52 @@ func applyMiddleware(handler http.Handler) http.Handler {
 	return handler
 }
 
-func pasteHandler(mc *minio.Client) http.Handler {
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		key := uuid.New().String()
-		ttl := 10 * time.Minute
+func handleRedirection(key string, mc *minio.Client, w http.ResponseWriter, r *http.Request) {
+	ttl := 2 * time.Minute
 
-		presignedUrl, err := mc.PresignedPutObject(os.Getenv("MINIO_BUCKET"), key, ttl)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Error(err)
-			return
-		}
+	presignedUrl, err := mc.PresignedPutObject(os.Getenv("MINIO_BUCKET"), key, ttl)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Error(err)
+		return
+	}
+	h := w.Header()
+	h.Set("Location", presignedUrl.String())
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func pasteHandler(mc *minio.Client, secret string) http.Handler {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := randomString(16)
+		signed := signString(key, secret)
+
 		returnUrl := fmt.Sprintf("%s/%s", r.Host, key)
 		h := w.Header()
-		h.Set("Location", presignedUrl.String())
 		h.Set("X-GET-URL", returnUrl)
-		w.WriteHeader(http.StatusTemporaryRedirect)
+		h.Set("X-SIGNED", signed)
+		handleRedirection(key, mc, w, r)
 		fmt.Fprintf(w, returnUrl)
+	})
+	return applyMiddleware(handler)
+}
+
+func editHandler(mc *minio.Client, secret string) http.Handler {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := mux.Vars(r)["key"]
+		signed := r.Header.Get("X-SIGNED")
+		if !validate(key, secret, signed) {
+			log.Info("Failed to validate HMAC")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		handleRedirection(key, mc, w, r)
 	})
 	return applyMiddleware(handler)
 }
 
 func getHandler(mc *minio.Client) http.Handler {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ttl := 10 * time.Minute
+		ttl := 2 * time.Minute
 		reqParams := make(url.Values)
 		presignedUrl, err := mc.PresignedGetObject(os.Getenv("MINIO_BUCKET"), r.URL.Path, ttl, reqParams)
 		if err != nil {
@@ -64,17 +112,24 @@ func getHandler(mc *minio.Client) http.Handler {
 }
 
 func serve() {
+	signingSecret := os.Getenv("SIGNING_SECRET")
+	if signingSecret == "" {
+		signingSecret = randomString(32)
+		log.Debugf("Created new signing secret: %s", signingSecret)
+	}
 	mc := getMinioClient()
 
 	r := mux.NewRouter()
 
-	r.Methods("PUT").Handler(pasteHandler(mc))
 	r.Methods("GET").Handler(getHandler(mc))
+	r.Methods("PUT").Path("/{key}").Handler(editHandler(mc, signingSecret))
+	r.Methods("PUT").Handler(pasteHandler(mc, signingSecret))
 
 	http.ListenAndServe(":3000", r)
 }
 
 func main() {
+	log.SetLevel(log.DebugLevel)
 	app := &cli.App{
 		Name:  "mcbin",
 		Usage: "Minio pasteboard",
